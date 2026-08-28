cd3022a6c47e85949e23acd331421b91b4b1afe5
Jesús Cantillo Guerrero <100422742+suscantillo@users.noreply.github.com>
2026-08-28 11:10:16 -0500

    Clean Dump

--- src/api.js
+++ src/api.js
@@ -1,17 +1,11 @@
-/**
- * Parte C — Programación asíncrona
- * Rama sugerida: feature/estadisticas
- *
- */
 
 const BASE_URL = "https://rickandmortyapi.com/api/character";
 
-// Estrategia 1: await dentro de un ciclo, una petición después de la otra.
+
 export async function fetchAllCharactersSequential() {
   throw new Error("fetchAllCharactersSequential: TODO (feature/estadisticas)");
 }
 
-// Estrategia 2 (la que debe usar la solución final): Promise.all().
 export async function fetchAllCharactersConcurrent() {
   throw new Error("fetchAllCharactersConcurrent: TODO (feature/estadisticas)");
 }
